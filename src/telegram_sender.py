53d734d05861fa5f8995f496ad7389f1da6bfd0e
Homar17 <omarbrizuela612@gmail.com>
2026-04-20 16:20:16 -0600

    Ocultar credenciales con dotenv

--- src/telegram_sender.py
+++ src/telegram_sender.py
@@ -1,12 +1,19 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-# Reemplaza con el Token que te dio BotFather
-TOKEN = "8646431458:AAErjZ-5O_mr3EEnQBul8jAY7eWGAKaB-wk"
+# Carga las variables ocultas del archivo .env al sistema
+load_dotenv()
 
-# Reemplaza con tu ID negativo obtenido de la API
-CHAT_ID = "-1003825074646" 
+# Extrae los valores de forma segura
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(message):
+    if not TOKEN or not CHAT_ID:
+        print("[ERROR TELEGRAM] Faltan credenciales. Revisa tu archivo .env")
+        return False
+        
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
@@ -17,7 +24,10 @@ def send_telegram_message(message):
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             print("[TELEGRAM] Notificacion enviada correctamente a tu dispositivo.")
+            return True
         else:
             print(f"[ERROR TELEGRAM] Fallo en el envio: {response.text}")
+            return False
     except Exception as e:
         print(f"[EXCEPCION TELEGRAM] No se pudo conectar: {e}")
+        return False
